cc8ccd9746e5fde7a05ecdd3d07cf150175ebf42
iranzi17 <50293498+iranzi17@users.noreply.github.com>
2025-08-29 08:24:53 +0200

    chore: unify Firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-npm install firebase
+
 const firebaseConfig = {
- apiKey: "AIzaSyC6UunoU3eeyT6vmR53VUgRbv10IlmWRH8",
+  apiKey: "AIzaSyC6UunoU3eeyT6vmR53VUgRbv10IlmWRH8",
   authDomain: "closeby-app-b70a1.firebaseapp.com",
   databaseURL: "https://closeby-app-b70a1-default-rtdb.firebaseio.com",
   projectId: "closeby-app-b70a1",
   storageBucket: "closeby-app-b70a1.appspot.com", // <-- FIXED
   messagingSenderId: "547769023139",
   appId: "1:547769023139:web:e9e3602166874fedd31d16",
-  measurementId: "G-MKL1L2K7Q1"
+  measurementId: "G-MKL1L2K7Q1",
 };
 
 const app = initializeApp(firebaseConfig);
